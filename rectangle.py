bda8f2608a56c899dbc3d6b5bc15b7ca17b3a41a
KryptonFox <kirieshka0785@gmail.com>
2025-10-14 17:59:46 +0300

    Rectangle and Triangle added

---
+++ rectangle.py
@@ -0,0 +1,24 @@
+def area(a, b):
+    """
+    Calculates the area of rectangle
+
+    Params:
+    a (int) - width
+    b (int) - heigth
+
+    Returns area of rectangle with width a and heigth b
+    """
+    return a * b
+
+
+def perimeter(a, b):
+    """
+    Calculates the perimeter of rectangle
+
+    Params:
+    a (int) - width
+    b (int) - heigth
+
+    Returns perimeter of rectangle with width a and heigth b
+    """
+    return 2 * (a + b)
---
+++ triangle.py
@@ -0,0 +1,25 @@
+def area(a, h):
+    """
+    Calculates the area of triangle
+
+    Params:
+    a (int) - base
+    h (int) - heigth
+
+    Returns area of triangle with width a base and h heigth
+    """
+    return a * h / 2
+
+
+def perimeter(a, b, c):
+    """
+    Calculates the perimeter of triangle
+
+    Params:
+    a (int) - 1st side
+    b (int) - 2nd side
+    c (int) - 3rd sides
+
+    Returns perimeter of triangle with a, b, c sides
+    """
+    return a + b + c
